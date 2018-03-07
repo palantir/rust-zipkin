d70368bdda8477b4a3507fe701a925ffded79e78
Steven Fackler <sfackler@gmail.com>
2018-03-06 22:04:08 -0800

    Update to Zipkin V2
    There are quite a few API changes here:
    
    * Binary annotations are gone.
    * Annotations no longer contain endpoints.
    * Annotations are now constructed directly via `new` and `now` rather
        than using a builder.
    * A Span contains new fields:
        * `kind` - An enum identifying the kind of span. This replaces the
            "special" `lc`, `cs`, `sr` etc annotations.
        * `shared` - Identifies if this span is joining an existing one
            created on a client. Server spans set this rather than omitting
            a timestamp and duration.
        * `localEndpoint` - The service's endpoint.
        * `remoteEndpoint` - The peer's endpoint.
        * `tags` - A String -> String map replacing binary annotations.
    * Endpoint's `serviceName` and `Span`'s `name` are now optional.
    * OpenSpan has a `name` setter, and that argument has been removed from
        the various `Tracer` methods.
    * OpenSpan's `server` and `client` methods have been removed in favor of
        the `kind` method.

--- zipkin/src/test.rs
+++ zipkin/src/test.rs
@@ -1,7 +1,7 @@
 use antidote::Mutex;
 use std::sync::Arc;
 
-use {Report, Span, Tracer, Endpoint};
+use {Endpoint, Report, Span, Tracer};
 
 struct TestReporter(Arc<Mutex<Vec<Span>>>);
 
@@ -16,17 +16,17 @@ fn detach() {
     let spans = Arc::new(Mutex::new(vec![]));
     let tracer = Tracer::builder()
         .reporter(Box::new(TestReporter(spans.clone())))
-        .build(Endpoint::builder().build("test"));
+        .build(Endpoint::builder().build());
 
-    let parent = tracer.new_trace("parent");
+    let parent = tracer.new_trace();
     let parent_trace = parent.context().trace_id();
     let parent_id = parent.context().span_id();
 
-    let mut detached = tracer.next_span("detached");
+    let mut detached = tracer.next_span();
     let detached_id = detached.context().span_id();
     detached.detach();
 
-    let child2 = tracer.next_span("child2");
+    let child2 = tracer.next_span();
     let child2_trace = child2.context().trace_id();
     let child2_id = child2.context().span_id();
 
