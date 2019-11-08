603e6f99d04a3b299f95e8297564f00044dc8b91
Steven Fackler <sfackler@palantir.com>
2019-11-08 15:48:37 -0500

    test .attach()

--- zipkin/src/test.rs
+++ zipkin/src/test.rs
@@ -20,7 +20,7 @@ fn init() {
 }
 
 #[test]
-fn detach() {
+fn detach_attach() {
     init();
 
     let parent = crate::new_trace().with_name("parent");
@@ -34,18 +34,25 @@ fn detach() {
     let child2_trace = child2.context().trace_id();
     let child2_id = child2.context().span_id();
 
-    drop(detached);
+    let attached = detached.attach();
+    let child3 = crate::next_span().with_name("child3");
+    let child3_id = child3.context().span_id();
+
+    drop(child3);
+    drop(attached);
     drop(child2);
     drop(parent);
 
     let spans = SPANS.with(|s| s.borrow().clone());
-    assert_eq!(spans.len(), 3);
-    assert_eq!(spans[0].id(), detached_id);
-    assert_eq!(spans[0].parent_id(), Some(parent_id));
-    assert_eq!(spans[1].trace_id(), child2_trace);
-    assert_eq!(spans[1].id(), child2_id);
+    assert_eq!(spans.len(), 4);
+    assert_eq!(spans[0].id(), child3_id);
+    assert_eq!(spans[0].parent_id(), Some(detached_id));
+    assert_eq!(spans[1].id(), detached_id);
     assert_eq!(spans[1].parent_id(), Some(parent_id));
-    assert_eq!(spans[2].trace_id(), parent_trace);
-    assert_eq!(spans[2].id(), parent_id);
-    assert_eq!(spans[2].parent_id(), None);
+    assert_eq!(spans[2].trace_id(), child2_trace);
+    assert_eq!(spans[2].id(), child2_id);
+    assert_eq!(spans[2].parent_id(), Some(parent_id));
+    assert_eq!(spans[3].trace_id(), parent_trace);
+    assert_eq!(spans[3].id(), parent_id);
+    assert_eq!(spans[3].parent_id(), None);
 }
