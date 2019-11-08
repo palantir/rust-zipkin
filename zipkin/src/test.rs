6453cf8cbe6325ee2b17dc3285aab054a5942e20
Steven Fackler <sfackler@palantir.com>
2019-11-08 16:00:34 -0500

    Test bind

--- zipkin/src/test.rs
+++ zipkin/src/test.rs
@@ -1,6 +1,7 @@
 use crate::sample::AlwaysSampler;
 use crate::{Endpoint, Report, Span};
 use std::cell::RefCell;
+use futures::executor;
 
 thread_local! {
     static SPANS: RefCell<Vec<Span>> = RefCell::new(vec![]);
@@ -23,19 +24,19 @@ fn init() {
 fn detach_attach() {
     init();
 
-    let parent = crate::new_trace().with_name("parent");
+    let parent = crate::new_trace();
     let parent_trace = parent.context().trace_id();
     let parent_id = parent.context().span_id();
 
-    let detached = crate::next_span().with_name("detached").detach();
+    let detached = crate::next_span().detach();
     let detached_id = detached.context().span_id();
 
-    let child2 = crate::next_span().with_name("child2");
+    let child2 = crate::next_span();
     let child2_trace = child2.context().trace_id();
     let child2_id = child2.context().span_id();
 
     let attached = detached.attach();
-    let child3 = crate::next_span().with_name("child3");
+    let child3 = crate::next_span();
     let child3_id = child3.context().span_id();
 
     drop(child3);
@@ -55,4 +56,35 @@ fn detach_attach() {
     assert_eq!(spans[3].trace_id(), parent_trace);
     assert_eq!(spans[3].id(), parent_id);
     assert_eq!(spans[3].parent_id(), None);
+}
+
+#[test]
+fn bind() {
+    init();
+
+    let future_root = crate::next_span();
+    let future_root_context = future_root.context();
+
+    let future = async {
+        let span = crate::next_span();
+        span.context()
+    };
+
+    let future = future_root.detach().bind(future);
+
+    let other_root = crate::next_span();
+    let other_root_context = other_root.context();
+
+    let future_context = executor::block_on(future);
+
+    drop(other_root);
+
+    let spans = SPANS.with(|s| s.borrow().clone());
+    assert_eq!(spans.len(), 3);
+    assert_eq!(spans[0].id(), future_context.span_id());
+    assert_eq!(spans[0].parent_id(), Some(future_root_context.span_id()));
+    assert_eq!(spans[1].id(), future_root_context.span_id());
+    assert_eq!(spans[1].parent_id(), None);
+    assert_eq!(spans[2].id(), other_root_context.span_id());
+    assert_eq!(spans[2].parent_id(), None);
 }
