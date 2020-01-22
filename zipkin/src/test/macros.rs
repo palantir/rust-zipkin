41bc5c847465d4d54bbd63fbe71749e24f912a67
Steven Fackler <sfackler@palantir.com>
2020-01-22 15:14:26 -0700

    Crate metadata

--- zipkin/src/test/macros.rs
+++ zipkin/src/test/macros.rs
@@ -1,3 +1,16 @@
+//  Copyright 2020 Palantir Technologies, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 use crate as zipkin; // hack to get the macro codegen to work in the same crate
 use crate::{spanned, test};
 use futures::executor;
